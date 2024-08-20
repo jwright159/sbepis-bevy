b47226317ac4c12355162670f5437da0f47529c8
Jady Wright <iamjwright159@gmail.com>
2024-08-20 18:34:16 -0400

    Cover up dead code

--- src/input.rs
+++ src/input.rs
@@ -44,6 +44,7 @@ pub fn button_event<Action: Actionlike + Copy, EventType: Event>(
 	.after(InputManagerSystem::ManualControl)
 }
 
+#[allow(dead_code)]
 pub fn button_input<Action: Actionlike + Copy>(
 	action: Action,
 ) -> impl Fn(Query<&ActionState<Action>>) -> bool {
