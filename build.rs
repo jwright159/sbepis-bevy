b18d3948634fc9619302d9f48b3485d507d13145
Jady Wright <iamjwright159@gmail.com>
2024-08-23 00:16:11 -0400

    Fix cross compilation to windows

--- build.rs
+++ build.rs
@@ -5,10 +5,16 @@ use {
 
 fn main() -> io::Result<()> {
 	if env::var_os("CARGO_CFG_WINDOWS").is_some() {
-		WindowsResource::new()
-			// This path can be absolute, or relative to your crate root.
-			.set_icon("assets/house.ico")
-			.compile()?;
+		let mut res = WindowsResource::new();
+
+		if cfg!(unix) {
+			res.set_toolkit_path("/usr/bin");
+			res.set_windres_path("x86_64-w64-mingw32-windres");
+		}
+
+		// This path can be absolute, or relative to your crate root.
+		res.set_icon("assets/house.ico");
+		res.compile()?;
 	}
 	Ok(())
 }
