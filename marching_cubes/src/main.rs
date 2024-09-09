f1d27a0e85372033439d8ca172acdd8dc94b6387
Jady Wright <iamjwright159@gmail.com>
2024-09-08 23:58:49 -0400

    Add a simple scene to the marching cubes demo

--- marching_cubes/src/main.rs
+++ marching_cubes/src/main.rs
@@ -1,3 +1,50 @@
+use bevy::prelude::*;
+
 fn main() {
-	println!("Hello, world!");
+	App::new()
+		.add_plugins((
+			DefaultPlugins,
+			bevy_inspector_egui::quick::WorldInspectorPlugin::new(),
+			bevy_panorbit_camera::PanOrbitCameraPlugin,
+		))
+		.add_systems(Startup, setup)
+		.run();
+}
+
+fn setup(
+	mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>,
+	mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+	commands.spawn((
+		Name::new("Camera"),
+		Camera3dBundle {
+			transform: Transform::from_xyz(4.0, 6.5, 8.0).looking_at(Vec3::ZERO, Vec3::Y),
+			..default()
+		},
+		bevy_panorbit_camera::PanOrbitCamera {
+			button_orbit: MouseButton::Left,
+			button_pan: MouseButton::Left,
+			modifier_pan: Some(KeyCode::ShiftLeft),
+			reversed_zoom: true,
+			..default()
+		},
+	));
+
+	commands.spawn((
+		Name::new("Cube"),
+		PbrBundle {
+			mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+			material: materials.add(Color::srgb_u8(124, 144, 255)),
+			..default()
+		},
+	));
+
+	commands.spawn((
+		Name::new("Light"),
+		PointLightBundle {
+			transform: Transform::from_xyz(4.0, 8.0, 4.0),
+			..default()
+		},
+	));
 }
